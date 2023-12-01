e75af3bc48bbd3347280ffd2f1ca03b3dc19cb70
Michael Cole <mcole042891@gmail.com>
2023-11-30 23:20:02 -0600

    Safe place after resetting to a new style

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, GUILD_ID_MICHAEL_DND } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !GUILD_ID_MICHAEL_DND) {
     throw new Error("Missing Environment Variables. Check .env file");
 }
 
 export const config = {
     DISCORD_TOKEN,
-    DISCORD_CLIENT_ID
+    DISCORD_CLIENT_ID,
+    GUILD_ID_MICHAEL_DND
 };
