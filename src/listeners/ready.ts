1eb8a4e733b0803bfe9242b11718e131feb8f8c3
Michael Cole <mcole042891@gmail.com>
2023-12-01 20:13:42 -0600

    Added an activity

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,4 +1,4 @@
-import { Client as DiscordClient, Events } from "discord.js";
+import { ActivityType, Client as DiscordClient, Events } from "discord.js";
 import { Commands } from "../Commands";
 
 export default (discordClient: DiscordClient): void => {
@@ -7,7 +7,13 @@ export default (discordClient: DiscordClient): void => {
             return;
         }
 
+        // Register all Slash Commands
         await discordClient.application.commands.set(Commands);
+
+        // Set Activity
+        discordClient.user.setActivity('in silence the cosmic currents emerging from the great abyss', {
+            type: ActivityType.Watching
+        });
 
         console.log(`${discordClient.user.displayName} is online`);
     })
